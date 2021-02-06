5f8fb4ae48e5a427c5830efec20f62c04db85e7c
sam bacha <sam@freighttrust.com>
2021-02-06 00:39:53 -0800

    Create nodejs_tuning.sh

---
+++ __pure__/stress-testing/nodejs/nodejs_tuning.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo echo '\nLANG="en_US.UTF-8"' >> /etc/default/locale && /etc/environment
+sudo echo '\nLANG="en_US.UTF-8"' >> /etc/default/locale && /etc/environment
+export NODE_OPTIONS='--trace-gc --trace-gc-ignore-scavenger --max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000'
