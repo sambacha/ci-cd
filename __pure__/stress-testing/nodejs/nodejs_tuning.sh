0f8f3253592979f3380cb92179366c31aa06bf85
sam bacha <sam@freighttrust.com>
2021-08-04 09:55:00 -0700

    Update nodejs_tuning.sh

--- __pure__/stress-testing/nodejs/nodejs_tuning.sh
+++ __pure__/stress-testing/nodejs/nodejs_tuning.sh
@@ -2,6 +2,6 @@
 sudo echo '\nLANG="en_US.UTF-8"' >> /etc/default/locale && /etc/environment
 sudo echo '\nLANG="en_US.UTF-8"' >> /etc/default/locale && /etc/environment
 export NODE_OPTIONS='--trace-gc --trace-gc-ignore-scavenger --max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000'
-
+npm config set scripts-prepend-node-path auto
 node -e 'console.log(`node heap limit = ${require("v8").getHeapStatistics().heap_size_limit / (1024 * 1024)} Mb`)'
 
